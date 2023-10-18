2fdad3e69494d41d7c1625e3246b0f162bf87e54
Mateusz Rzeczkowski <mateusz@Mateuszs-MacBook-Pro.local>
2023-10-18 21:51:54 +0200

    major changes

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"boids/vector"
 	"fmt"
 	"image"
 	"image/color"
 	"math"
 	"math/rand"
-	"sync"
 	"time"
 
 	"github.com/faiface/pixel"
@@ -15,27 +15,22 @@ import (
 )
 
 const (
-	WindowWidth  = 1280.0
-	WindowHeight = 720.0
-	BoidsCount   = 1000
+	WindowWidth  = 1440.0
+	WindowHeight = 900.0
+	BoidsCount   = 100
 )
 
 // Components
-type Position struct {
-	x, y float64
-}
-
-type Velocity struct {
-	dx, dy float64
-}
+type Vector2D = vector.Vector2D
 
 type Boid struct{}
 
 type Entity struct {
-	ID       int
-	Position *Position
-	Velocity *Velocity
-	Boid     *Boid
+	ID           int
+	Position     Vector2D
+	Velocity     Vector2D
+	Acceleration Vector2D
+	Boid         *Boid
 }
 
 type System interface {
@@ -43,14 +38,16 @@ type System interface {
 }
 
 // Movement System
-type MovementSystem struct{}
+type MovementSystem struct {
+	maxSpeed float64
+}
 
 func (ms *MovementSystem) Update(entities []*Entity) {
 	for _, entity := range entities {
-		if entity.Position != nil && entity.Velocity != nil {
-			entity.Position.x += entity.Velocity.dx
-			entity.Position.y += entity.Velocity.dy
-		}
+		entity.Position.Add(entity.Velocity)
+		entity.Velocity.Add(entity.Acceleration)
+		entity.Velocity.Limit(ms.maxSpeed)
+		entity.Acceleration.Multiply(0)
 	}
 }
 
@@ -60,6 +57,7 @@ type BoidsSystem struct {
 	alignmentFactor   float64
 	separationFactor  float64
 	neighborhoodRange float64
+	maxForce          float64
 	maxSpeed          float64
 }
 
@@ -69,87 +67,103 @@ func (bs *BoidsSystem) Update(entities []*Entity) {
 			continue
 		}
 
-		avgPosition := &Position{}
-		avgVelocity := &Velocity{}
-		avoidance := &Velocity{}
-		neighborCount := 0
+		alignmentSteering := Vector2D{}
+		cohesionSteering := Vector2D{}
+		separationSteering := Vector2D{}
+		var neighborCount float64 = 0.0
 
 		for _, other := range entities {
 			if other.Boid == nil || boid.ID == other.ID {
 				continue
 			}
-			d := distance(boid.Position, other.Position)
+			d := boid.Position.Distance(other.Position)
 			if d < bs.neighborhoodRange {
-				avgPosition.x += other.Position.x
-				avgPosition.y += other.Position.y
-				avgVelocity.dx += other.Velocity.dx
-				avgVelocity.dy += other.Velocity.dy
+				alignmentSteering.Add(other.Velocity)
+				cohesionSteering.Add(other.Position)
 
-				separationDistance := bs.neighborhoodRange * 0.3 // Adjust this value as needed
-				if d < separationDistance {
-					avoidance.dx += boid.Position.x - other.Position.x
-					avoidance.dy += boid.Position.y - other.Position.y
-				}
+				diff := boid.Position
+				diff.Subtract(other.Position)
+				diff.Divide(d) // Not squared?
+				separationSteering.Add(diff)
 
 				neighborCount++
 			}
 		}
 
 		if neighborCount > 0 {
-			avgPosition.x /= float64(neighborCount)
-			avgPosition.y /= float64(neighborCount)
-			avgVelocity.dx /= float64(neighborCount)
-			avgVelocity.dy /= float64(neighborCount)
 
-			cohesion := &Velocity{
-				dx: (avgPosition.x - boid.Position.x) * bs.cohesionFactor,
-				dy: (avgPosition.y - boid.Position.y) * bs.cohesionFactor,
+			alignmentSteering.Divide(neighborCount)
+			alignmentSteering.SetMagnitude(bs.maxSpeed)
+			alignmentSteering.Subtract(boid.Velocity)
+			alignmentSteering.Limit(bs.maxForce)
+
+			cohesionSteering.Divide(neighborCount)
+			cohesionSteering.Subtract(boid.Position)
+			cohesionSteering.SetMagnitude(bs.maxSpeed)
+			cohesionSteering.Subtract(boid.Velocity)
+			cohesionSteering.Limit(bs.maxForce)
+
+			separationSteering.Divide(neighborCount)
+			separationSteering.SetMagnitude(bs.maxSpeed)
+			separationSteering.Subtract(boid.Velocity)
+			separationSteering.SetMagnitude(bs.maxForce)
+
+			alignmentSteering.Multiply(bs.alignmentFactor)
+			cohesionSteering.Multiply(bs.cohesionFactor)
+			separationSteering.Multiply(bs.separationFactor)
+
+			boid.Acceleration.Add(alignmentSteering)
+			boid.Acceleration.Add(cohesionSteering)
+			boid.Acceleration.Add(separationSteering)
+			boid.Acceleration.Divide(3) // WHY?
+		}
+
+		const boundaryMargin = 50.0
+		const boundaryForce = 0.2
+
+		const bounce = false
+
+		if bounce {
+			// If boid is near left boundary
+			if boid.Position.X < boundaryMargin {
+				boid.Velocity.X += boundaryForce
 			}
-			alignment := &Velocity{
-				dx: (avgVelocity.dx - boid.Velocity.dx) * bs.alignmentFactor,
-				dy: (avgVelocity.dy - boid.Velocity.dy) * bs.alignmentFactor,
-			}
-			separation := &Velocity{
-				dx: avoidance.dx * bs.separationFactor,
-				dy: avoidance.dy * bs.separationFactor,
+
+			// If boid is near right boundary
+			if boid.Position.X > WindowWidth-boundaryMargin {
+				boid.Velocity.X -= boundaryForce
 			}
 
-			boid.Velocity.dx += cohesion.dx + alignment.dx + separation.dx
-			boid.Velocity.dy += cohesion.dy + alignment.dy + separation.dy
+			// If boid is near bottom boundary
+			if boid.Position.Y < boundaryMargin {
+				boid.Velocity.Y += boundaryForce
+			}
 
-			limitMagnitude(boid.Velocity, bs.maxSpeed)
+			// If boid is near top boundary
+			if boid.Position.Y > WindowHeight-boundaryMargin {
+				boid.Velocity.Y -= boundaryForce
+			}
+		} else {
+			// If boid crosses left boundary
+			if boid.Position.X < 0 {
+				boid.Position.X = WindowWidth
+			}
+
+			// If boid crosses right boundary
+			if boid.Position.X > WindowWidth {
+				boid.Position.X = 0
+			}
+
+			// If boid crosses bottom boundary
+			if boid.Position.Y < 0 {
+				boid.Position.Y = WindowHeight
+			}
+
+			// If boid crosses top boundary
+			if boid.Position.Y > WindowHeight {
+				boid.Position.Y = 0
+			}
 		}
-
-		const boundaryMargin = 30.0
-		const boundaryForce = 0.5
-
-		// If boid is near left boundary
-		if boid.Position.x < boundaryMargin {
-			boid.Velocity.dx += boundaryForce
-		}
-
-		// If boid is near right boundary
-		if boid.Position.x > WindowWidth-boundaryMargin {
-			boid.Velocity.dx -= boundaryForce
-		}
-
-		// If boid is near bottom boundary
-		if boid.Position.y < boundaryMargin {
-			boid.Velocity.dy += boundaryForce
-		}
-
-		// If boid is near top boundary
-		if boid.Position.y > WindowHeight-boundaryMargin {
-			boid.Velocity.dy -= boundaryForce
-		}
-	}
-}
-
-func steerTowardsCenter(position *Position, windowCenter pixel.Vec) *Velocity {
-	const centerForce = 0.001
-	return &Velocity{
-		dx: (windowCenter.X - position.x) * centerForce,
-		dy: (windowCenter.Y - position.y) * centerForce,
 	}
 }
 
@@ -159,35 +173,17 @@ type World struct {
 }
 
 func (w *World) Update() {
-	var wg sync.WaitGroup
-
 	for _, system := range w.Systems {
-		wg.Add(1) // Increment the WaitGroup counter for each system.
-
-		go func(s System) {
-			defer wg.Done() // Decrement the WaitGroup counter once the system finishes updating.
-
-			s.Update(w.Entities)
-		}(system)
+		system.Update(w.Entities)
 	}
-
-	wg.Wait() // Wait for all systems to finish updating.
 }
 
 func NewWorld() *World {
 	return &World{}
 }
 
-func distance(p1, p2 *Position) float64 {
-	return math.Sqrt(math.Pow(p2.x-p1.x, 2) + math.Pow(p2.y-p1.y, 2))
-}
-
-func limitMagnitude(v *Velocity, max float64) {
-	mag := math.Sqrt(v.dx*v.dx + v.dy*v.dy)
-	if mag > max {
-		v.dx = (v.dx / mag) * max
-		v.dy = (v.dy / mag) * max
-	}
+func distance(p1, p2 *Vector2D) float64 {
+	return math.Sqrt(math.Pow(p2.X-p1.X, 2) + math.Pow(p2.Y-p1.Y, 2))
 }
 
 func run() {
@@ -205,29 +201,35 @@ func run() {
 
 	for i := 0; i < BoidsCount; i++ {
 		angle := rand.Float64() * 2 * math.Pi
-		speed := rand.Float64()*6 + 2 // speeds between 2 and 8
+		speed := rand.Float64()
 		boid := &Entity{
-			ID:       i,
-			Position: &Position{x: rand.Float64() * WindowWidth, y: rand.Float64() * WindowHeight},
-			Velocity: &Velocity{dx: math.Cos(angle) * speed, dy: math.Sin(angle) * speed},
-			Boid:     &Boid{},
+			ID:           i,
+			Position:     Vector2D{X: rand.Float64() * WindowWidth, Y: rand.Float64() * WindowHeight},
+			Velocity:     Vector2D{X: math.Cos(angle) * speed, Y: math.Sin(angle) * speed},
+			Acceleration: Vector2D{X: 0, Y: 0},
+			Boid:         &Boid{},
 		}
 		world.Entities = append(world.Entities, boid)
 	}
 
+	movementSystem := &MovementSystem{
+		maxSpeed: 1,
+	}
+
 	boidsSystem := &BoidsSystem{
-		cohesionFactor:    0.002,
-		alignmentFactor:   0.05,
-		separationFactor:  0.1,
-		neighborhoodRange: 40,
+		cohesionFactor:    0.9,
+		alignmentFactor:   1.0,
+		separationFactor:  1.2,
+		neighborhoodRange: 75,
+		maxForce:          1.0,
 		maxSpeed:          4,
 	}
 
-	world.Systems = append(world.Systems, &MovementSystem{}, boidsSystem)
+	world.Systems = append(world.Systems, movementSystem, boidsSystem)
 
 	for !win.Closed() {
 		// Runtime parameter adjustments
-		adjustFactor := 0.001 // Adjust by a small amount for fine control
+		adjustFactor := 0.01 // Adjust by a small amount for fine control
 		if win.JustPressed(pixelgl.KeyW) {
 			boidsSystem.cohesionFactor += adjustFactor
 			fmt.Printf("Cohesion Factor: %f\n", boidsSystem.cohesionFactor)
@@ -275,9 +277,9 @@ func run() {
 		boidSprite := createIsoscelesTriangleSprite(10, 20) // Adjust baseLength and height as needed
 		for _, entity := range world.Entities {
 			if entity.Boid != nil {
-				pos := pixel.V(entity.Position.x, entity.Position.y)
+				pos := pixel.V(entity.Position.X, entity.Position.Y)
 
-				angle := math.Atan2(entity.Velocity.dy, entity.Velocity.dx) + math.Pi/2
+				angle := math.Atan2(entity.Velocity.Y, entity.Velocity.X) + math.Pi/2
 				mat := pixel.IM.Moved(pos).Rotated(pos, angle)
 
 				boidSprite.Draw(win, mat)
@@ -325,24 +327,6 @@ func insideTriangle(pt, v1, v2, v3 pixel.Vec) bool {
 
 func sign(p1, p2, p3 pixel.Vec) float64 {
 	return (p1.X-p3.X)*(p2.Y-p3.Y) - (p2.X-p3.X)*(p1.Y-p3.Y)
-}
-
-func createCircleSprite(radius float64) *pixel.Sprite {
-	img := image.NewRGBA(image.Rect(0, 0, int(radius*2)+1, int(radius*2)+1))
-	cen := pixel.V(radius, radius)
-	col := color.RGBA{255, 0, 0, 255} // Red color for our boid circles
-
-	for y := 0.0; y < radius*2; y++ {
-		for x := 0.0; x < radius*2; x++ {
-			pos := pixel.V(x, y)
-			if cen.To(pos).Len() < radius {
-				img.Set(int(x), int(y), col)
-			}
-		}
-	}
-
-	pic := pixel.PictureDataFromImage(img)
-	return pixel.NewSprite(pic, pic.Bounds())
 }
 
 func main() {
